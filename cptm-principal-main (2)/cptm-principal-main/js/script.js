5a7c7b18446189359f3a38a1e9e2c035ced45785
ana <annnasantos4530@gmail.com>
2024-10-01 16:41:21 -0300

    Add files via upload

--- cptm-principal-main (2)/cptm-principal-main/js/script.js
+++ cptm-principal-main (2)/cptm-principal-main/js/script.js
@@ -1,1 +1,7 @@
-<button onclick="scrollToTop()">Topo</button> //volta p top da pág
+{/* <button onclick="scrollToTop()">Topo</button> //volta p top da pág */}
+
+$("#button").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#anchor").offset().top
+    }, 2000);
+  });
